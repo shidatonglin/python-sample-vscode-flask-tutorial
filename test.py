002d012394842abbeb6622a4f9f1b42c20904c7a
shidatonglin <shidatonglin@163.com>
2020-02-26 19:06:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,25 @@
+import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-#$env:CHROMEWEBDRIVER
-driver_path = ''
-url = 'www.biying.com'
+class PythonOrgSearch(unittest.TestCase):
 
-def test():
-    print('This is print from azure pipeline')
-    driver = webdriver.Chrome()
-    driver.get(url)
+    def setUp(self):
+        self.driver = webdriver.Chrome()
 
-if __name__ == '__main__':
-    test()
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    print('Start selenium test')
+    unittest.main()
